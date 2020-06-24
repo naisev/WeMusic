137834a0fd98e6448c5d49231602697654951ffb
Naise <2387744807@qq.com>
2020-06-24 23:24:06 +0800

    add PlatformListModel and PlatformInfoModel

---
+++ Model/DbModel/PlatformInfoModel.cs
@@ -0,0 +1,35 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeMusic.Enum;
+using WeMusic.Interface;
+
+namespace WeMusic.Model.DbModel
+{
+    [SugarTable("PlatformInfo")]
+    public class PlatformInfoModel : ISongList
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public string Id { get; set; }
+
+        [SugarColumn(IsIgnore = true)]
+        public IMusic[] Musics { get; set; }
+
+        public string CoverUrl { get; set; }
+
+        public MusicSource Origin { get; set; }
+
+        [SugarColumn(IsIgnore = true)]
+        public string SourceName { get; set; }
+
+        public string Title { get; set; }
+
+        public PlatformInfoModel()
+        {
+
+        }
+    }
+}
---
+++ Model/DbModel/PlatformListModel.cs
@@ -0,0 +1,22 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeMusic.Model.DbModel
+{
+    [SugarTable("PlatformList")]
+    public class PlatformListModel
+    {
+        public string PlatformId { get; set; }
+
+        public string MusicId { get; set; }
+
+        public PlatformListModel()
+        {
+
+        }
+    }
+}
